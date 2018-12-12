78b0ccf3ab32bc98fc36f918fd0077253c9c9a87
rodomac <rodopos@nebular.tv>
2018-12-12 02:37:37 +0100

    make-iso bug

--- lib/portron.default/make_iso.sh
+++ lib/portron.default/make_iso.sh
@@ -9,12 +9,12 @@ echo "This script will create Porteus Kiosk ISO from files in current directory.
 # UEFI check:
 [ -e boot/isolinux/efi/efiboot.img ] && efi="-eltorito-alt-boot -eltorito-platform 0xEF -eltorito-boot boot/isolinux/efi/efiboot.img -no-emul-boot"
 
-mkisofs -o ../Porteus-Kiosk.iso -l -J -joliet-long -R -D -A "Kiosk" \
+mkisofs -o $1 -l -J -joliet-long -R -D -A "Kiosk" \
 -V "Kiosk" -no-emul-boot -boot-info-table -boot-load-size 4 \
 -b boot/isolinux/isolinux.bin -c boot/isolinux/isolinux.boot $efi . > /dev/null 2>&1
 
 if [ $? -eq 0 ]; then
-    echo "Saved as [1;33m`pwd | rev | cut -d/ -f2- | rev`/Porteus-Kiosk.iso[0m."
+    echo "Saved as [1;33m`pwd | rev | cut -d/ -f2- | rev`/${1}[0m."
 else
     echo "Something went wrong - could not create the ISO. Please use 'mkisofs' utility from latest 'cdrtools' package rather than 'cdr-kit'."
     echo "You can download 'mkisofs' utility directly from this location: http://porteus-kiosk.org/public/files/mkisofs"
